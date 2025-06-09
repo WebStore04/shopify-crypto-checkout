121a93019ee962cefcfbd8d34874588c7258343b
roman <dzulroman553@gmail.com>
2025-06-10 01:07:44 +0300

    feat: restructure full project directory and add admin-panel project

---
+++ client/admin-panel/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminPanel from "./components/AdminPanel";
+
+const App: React.FC = () => {
+  return <AdminPanel />;
+};
+
+export default App;
---
+++ client/admin-panel/src/components/AdminPanel.tsx
@@ -0,0 +1,7 @@
+export default function AdminPanel() {
+  return (
+    <div className="max-w-sm mx-auto mt-20 p-4 bg-white rounded-md shadow-md">
+      <h1>Admin Panel</h1>
+    </div>
+  );
+}
