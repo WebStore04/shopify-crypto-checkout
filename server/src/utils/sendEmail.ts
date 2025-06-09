90ea40cc8117b6418e37eb426bac1c0bce61fd44
roman <dzulroman553@gmail.com>
2025-06-09 18:36:26 +0300

    refactor: update variable

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -4,14 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const emailUser = process.env.EMAIL_USER!;
-const pass = process.env.EMAIL_PASSWORD!;
+const emailPsword = process.env.EMAIL_PASSWORD!;
+const emailHost = process.env.EMAIL_HOST!;
+const emailPort = parseInt(process.env.EMAIL_PORT!);
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
+  host: emailHost,
+  port: emailPort,
   auth: {
     user: emailUser,
-    pass: pass,
+    pass: emailPsword,
   },
 });
 
